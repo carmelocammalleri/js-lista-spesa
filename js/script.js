3baf3235074872d1e3f053a94aae141ef22cc624
Melozzo <carmecamma1995@gmail.com>
2023-09-14 14:16:26 +0200

    [edit]: while

--- js/script.js
+++ js/script.js
@@ -4,11 +4,27 @@ const listGroup = document.getElementById ('list');
 const listEl = [
   'acqua',
   'pane',
-  'sale'
+  'sale',
+  'carote',
+  'cipolle'
 ]
 
 //2.
-const addEl = prompt('Aggiungi cose da comprare');
+// const addEl = prompt('Aggiungi cose da comprare');
+// listEl.push(addEl);
+
+//3.
+let i = 0;
+let message = "";
+
+while(i < listEl.length){
+  let spesa = listEl[i];
+  message += `<li>${spesa}</li>`; i++;
+  listGroup.innerHTML=message;
+}
+console.log(message);
+
+
 
 
 
