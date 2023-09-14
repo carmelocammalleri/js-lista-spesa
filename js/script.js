3c544d647bb658d874cb1c2ab37b766194b22d8e
Melozzo <carmecamma1995@gmail.com>
2023-09-14 13:47:21 +0200

    [edit]: js consts

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const listGroup = document.getElementById ('list');
+
+//1.
+const listEl = [
+  'acqua',
+  'pane',
+  'sale'
+]
+
+//2.
+const addEl = prompt('Aggiungi cose da comprare');
+
+
+
+
